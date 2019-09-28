ea076589ccb8c9b6d2dc41b08e3b888f7cfb7f4c
Tyler Westland <tylerofthewest@gmail.com>
2019-09-28 11:34:12 -0400

    Initial Commit
    It's functional, and can thus be slightly taken apart for demonstration
    purposes.

---
+++ app/src/main/java/com/westland/tyler/randomkindness/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.westland.tyler.randomkindness
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+// Our content xmls, importing them allows us to access the views (pieces) as variables
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            showRandomKindPhrase()
+        }
+
+        showRandomKindPhrase()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_random_kind_phrase -> {
+                showRandomKindPhrase()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun randomKindPhrase(): String {
+        // Get the array of kind phrases
+        val youArePieces = resources.getStringArray(R.array.you_are_pieces)
+        // Pick a random index within the array
+        val pieceIndex = (Math.random() * youArePieces.size).toInt()
+        // Get the string that the phrase will be inserted into
+        // English: "You are\n%1$s"
+        // %1$s is the part that is replace according to String.format()'s rules.
+        val youArePhrase = resources.getString(R.string.you_are_phrase)
+
+        return String.format(youArePhrase, youArePieces[pieceIndex])
+    }
+
+    private fun showRandomKindPhrase() {
+        // cute_phrase is a TextView object inside of content_main.xml
+        // We imported it at the top of the file, so we can just access it.
+        kind_phrase.text =  this.randomKindPhrase()
+    }
+}
